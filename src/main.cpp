c2aa176367def5f31248c1d51a53ea9fd0bc6f1d
Elio6174 <elseismylife@gmail.com>
2026-05-24 00:10:27 -0600

    antes de ambios

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,24 @@
 #include <iostream>
 
 int main(){
-	cv::Mat img = cv::imread("../images/IMG_1.jpg");
+  cv::Mat img = cv::imread("images/IMG_1.jpg");
   cv::Mat gray, blurred, edges;
 
   cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
   cv::GaussianBlur(gray, blurred, cv::Size(5, 5), 0);
   cv::Canny(blurred, edges, 50, 150);
 
-  //cv::imshow("Original", img);
-  //cv::imshow("GrayScale", gray);
+  cv::imshow("Original", img);
+  cv::imshow("GrayScale", gray);
   cv::imshow("Edges", edges);
-  
+
+
+  cv::imwrite("imgEdges.png", edges);  
+  cv::imwrite("imgGray.png", gray);  
+
+
   cv::waitKey(0);
   cv::destroyAllWindows();
-
-	return 0;
+  
+  return 0;
 }
