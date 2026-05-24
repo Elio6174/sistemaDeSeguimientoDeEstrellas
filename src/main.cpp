c7588f7df1f6cb10b331b7152a6e63683d07e0a5
Elio6174 <elseismylife@gmail.com>
2026-05-24 02:38:07 -0600

    estudio de imagenes en hsv

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,17 @@
 
 int main(){
   cv::Mat img = cv::imread("images/IMG_1.jpg");
-  cv::Mat gray, blurred, edges;
+  cv::Mat hsv, mask, mask1, mask2;
 
-  cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
-  cv::GaussianBlur(gray, blurred, cv::Size(5, 5), 0);
-  cv::Canny(blurred, edges, 50, 150);
+  cv::cvtColor(img, hsv, cv::COLOR_BGR2HSV);
+  
+  cv::inRange(hsv, cv::Scalar(0, 120, 120), cv::Scalar(15, 255, 255), mask1);
+  cv::inRange(hsv, cv::Scalar(165, 120, 120), cv::Scalar(179, 255, 255), mask2);  
+//  cv::circle(mask, cv::Point(0, 0), 50, cv::Scalar(0, 0, 255), 3);
+//  cv::circle(mask, cv::Point(10, 10), 15, cv::Scalar(255, 255, 0), cv::FILLED);
 
-  cv::imshow("Original", img);
-  cv::imshow("GrayScale", gray);
-  cv::imshow("Edges", edges);
-
-
-  cv::imwrite("imgEdges.png", edges);  
-  cv::imwrite("imgGray.png", gray);  
-
+  cv::bitwise_or(mask1, mask2, mask);
+  cv::imwrite("imgOutPut.png", mask);  
 
   cv::waitKey(0);
   cv::destroyAllWindows();
